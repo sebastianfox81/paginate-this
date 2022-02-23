45accc113992b10a772c2685a9d7598a9423ef9b
Sebastian <sebastianfox81@gmail.com>
2022-02-23 14:43:00 -0500

    Implemented handlePage functionality

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,11 @@ function App() {
   useEffect(() => {
     if (loading) return
     setFollowers(data[page])
-  }, [loading])
+  }, [loading, page])
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
 
   return (
     <main>
@@ -26,6 +30,19 @@ function App() {
             )
           })}
         </div>
+        {!loading && <div className="btn-container">
+          {
+            data.map((item, index) => {
+              return (
+                <button key={index} className={`page-btn ${index === page ? 'active-btn' : null}`} onClick={() => handlePage(index)}>
+                  { index + 1 }
+                </button>
+              )
+            })
+          }
+        </div>
+        }
+
       </section>
 
     </main>
